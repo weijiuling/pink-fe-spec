0ae504e03121e3e3eea3ffd32e025c9a70ee1742
Jolin <670618924@qq.com>
2024-11-08 23:56:50 +0800

    up

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/weijiuling/Desktop/pink-fe-spec/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello VuePress889999999\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1731078175000,\"contributors\":[{\"name\":\"Jolin\",\"email\":\"670618924@qq.com\",\"commits\":3}]},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"Hello VuePress889999999\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1731079682000,\"contributors\":[{\"name\":\"Jolin\",\"email\":\"670618924@qq.com\",\"commits\":4,\"url\":\"https://github.com/Jolin\"}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,4 @@
-/*
- * @Author: Jolin 670618924@qq.com
- * @Date: 2024-11-08 22:49:13
- * @LastEditors: Jolin 670618924@qq.com
- * @LastEditTime: 2024-11-08 22:49:32
- * @FilePath: /pink-fe-spec/docs/.vuepress/config.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
+
 import { viteBundler } from '@vuepress/bundler-vite'
 import { defaultTheme } from '@vuepress/theme-default'
 import { defineUserConfig } from 'vuepress'
@@ -13,4 +6,5 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
+  base: "/pink-fe-specs/",
 })
